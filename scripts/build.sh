ba1ffa760ea694e9613eb5d848ee17e16ad0cb2a
Lobooooooo14 <88998991+Lobooooooo14@users.noreply.github.com>
2023-12-10 22:13:30 -0300

    feat: added release and debug builds

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+is_release=false
+
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in
+        --release)
+            is_release=true
+            shift
+            ;;
+        --debug)
+            is_release=false
+            shift
+            ;;
+        *)
+            shift
+            ;;
+    esac
+done
+
 if [ "$WORKFLOW" = true ]; then
     echo -e "\033[1;33mRunning in a workflow. Using workflow ambient variables."
 else
@@ -11,9 +30,16 @@ else
     fi
 fi
 
-npx cap build android \
-  --keystorepath "keystore/release.keystore" \
-  --keystorepass $KEYSTOREPASS \
-  --keystorealiaspass $KEYSTOREALIASPASS \
-  --keystorealias $KEYSTOREALIAS \
-  --androidreleasetype APK
+if [ "$is_release" = true ]; then
+  npx cap build android \
+    --keystorepath "keystore/release.keystore" \
+    --keystorepass $KEYSTOREPASS \
+    --keystorealiaspass $KEYSTOREALIASPASS \
+    --keystorealias $KEYSTOREALIAS \
+    --androidreleasetype APK
+
+else
+  cd ./android/
+  ./gradlew assembleDebug
+  cd ..
+fi
